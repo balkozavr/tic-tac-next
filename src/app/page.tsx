897f66d3b15f341b9fc6c9631fec53e0c091da7b
balkovoyva <balkovoyva@devtrix.ru>
2025-03-09 13:17:27 +0500

    prisma, docker-compose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { Button } from "@/shared/ui/button"
+import { prisma } from "@/shared/lib/db"
+import { Card, CardTitle } from "@/shared/ui/card"
 
-export default function Home() {
-  return <Button variant={"destructive"}>home</Button>
+export default async function Home() {
+  const games = await prisma.game.findMany()
+  return (
+    <>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
+    </>
+  )
 }
